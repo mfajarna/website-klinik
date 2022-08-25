1b89401b37d4a63d897e22f40f2a2538fa0716ea
mfajarna <invasionfajar@gmail.com>
2022-08-25 21:04:37 +0700

    master

--- resources/views/includes/Landing/script.blade.php
+++ resources/views/includes/Landing/script.blade.php
@@ -1,13 +1,13 @@
     <!-- JAVASCRIPT -->
-    <script src="{{ asset('/assets/libs/jquery/jquery.min.js')}}"></script>
-    <script src="{{ asset('/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{ asset('/assets/libs/metismenu/metisMenu.min.js')}}"></script>
-    <script src="{{ asset('/assets/libs/simplebar/simplebar.min.js')}}"></script>
-    <script src="{{ asset('/assets/libs/node-waves/waves.min.js')}}"></script>
-    <script src="{{ asset('/assets/libs/feather-icons/feather.min.js')}}"></script>
+    <script src="{{ url('/public/assets/libs/jquery/jquery.min.js')}}"></script>
+    <script src="{{ url('/public/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ url('/public/assets/libs/metismenu/metisMenu.min.js')}}"></script>
+    <script src="{{ url('/public/assets/libs/simplebar/simplebar.min.js')}}"></script>
+    <script src="{{ url('/public/assets/libs/node-waves/waves.min.js')}}"></script>
+    <script src="{{ url('/public/assets/libs/feather-icons/feather.min.js')}}"></script>
     <!-- pace js -->
-    <script src="{{ asset('/assets/libs/pace-js/pace.min.js')}}"></script>
+    <script src="{{ url('/public/assets/libs/pace-js/pace.min.js')}}"></script>
 
-    <script src="{{ asset('/assets/js/pages/pass-addon.init.js')}}"></script>
+    <script src="{{ url('/public/assets/js/pages/pass-addon.init.js')}}"></script>
 
-    <script src="{{ asset('/assets/js/pages/feather-icon.init.j')}}s"></script>
+    <script src="{{ url('/public/assets/js/pages/feather-icon.init.js')}}"></script>
--- resources/views/includes/Landing/style.blade.php
+++ resources/views/includes/Landing/style.blade.php
@@ -1,12 +1,12 @@
 <!-- App favicon -->
-<link rel="shortcut icon" href="{{ url('/assets/images/LogoKlinikCS.jpg')}}">
+<link rel="shortcut icon" href="{{ url('/public/assets/images/LogoKlinikCS.jpg')}}">
 
 <!-- preloader css -->
-<link rel="stylesheet" href="{{ url('/assets/css/preloader.min.css')}}" type="text/css" />
+<link rel="stylesheet" href="{{ url('/public/assets/css/preloader.min.css')}}" type="text/css" />
 
 <!-- Bootstrap Css -->
-<link href="{{ url('/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+<link href="{{ url('/public/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
-<link href="{{ url('/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{ url('/public/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
 <!-- App Css-->
-<link href="{{ url('/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
+<link href="{{ url('/public/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
